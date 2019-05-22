eca2a4d5e3ba048ba0d005d9a99f4d9da885cfd8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-22 14:47:59 +0000

    Deploy maikel-bakker/recipes-app to github.com/maikel-bakker/recipes-app.git:gh-pages

---
+++ precache-manifest.4e68a50c124f6fbdea7a74b2499b0452.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7235024146bdc41884f60f53541c1b39",
+    "url": "/recipes-app/index.html"
+  },
+  {
+    "revision": "a5b472c00969b2a2d9c8",
+    "url": "/recipes-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1c3e89f416500b504b6d",
+    "url": "/recipes-app/static/js/2.ee2a9de1.chunk.js"
+  },
+  {
+    "revision": "a5b472c00969b2a2d9c8",
+    "url": "/recipes-app/static/js/main.660a0370.chunk.js"
+  },
+  {
+    "revision": "dfa723fb594534a9c68a",
+    "url": "/recipes-app/static/js/runtime~main.fdf092f1.js"
+  }
+]);
